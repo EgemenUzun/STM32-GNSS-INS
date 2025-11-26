de43a245bfea40cbc4233fe39fc607300aa30f34
egemen.uzun <egemen.uzun@esensi.com.tr>
2025-11-26 10:53:42 +0300

    refactor: enhance complementary filter and heading calculation

--- Core/Src/complementary_filter.c
+++ Core/Src/complementary_filter.c
@@ -13,10 +13,11 @@
 uint32_t timer = 0;
 float acc_roll = 0, acc_pitch = 0;
 float gyro_roll = 0, gyro_pitch = 0, gyro_yaw = 0;
-static float alpha = 0.98f;
+float alpha = 0.98f;
+float alpha_heading = 0.97f;
 double dt = 0;
 
-void Calculate_Euler_Angles(MPU6050_t *mpu6050, uint32_t tick) {
+void Calculate_Euler_Angles(MPU6050_t *mpu6050, uint32_t tick, HMC5883L_t *hmc) {
 	if (timer == 0) {
 		dt = 0; // start time axis at 0
 	} else {
@@ -28,13 +29,22 @@ void Calculate_Euler_Angles(MPU6050_t *mpu6050, uint32_t tick) {
     acc_pitch = atan2(-mpu6050->accel[0], sqrt(mpu6050->accel[1]*mpu6050->accel[1] + mpu6050->accel[2]*mpu6050->accel[2])) * RAD_TO_DEG;
 	printf("dt: %f\n", dt);
 
+	mpu6050->gyro[0] = (float)(mpu6050->gyro_raw[0] - mpu6050->gyro_offset[0]) / mpu6050->gyro_sensitivity;
+	mpu6050->gyro[1] = (float)(mpu6050->gyro_raw[1] - mpu6050->gyro_offset[1]) / mpu6050->gyro_sensitivity;
+	mpu6050->gyro[2] = (float)(mpu6050->gyro_raw[2] - mpu6050->gyro_offset[2]) / mpu6050->gyro_sensitivity;
+
+	mpu6050->accel[0] = (float)(mpu6050->accel_raw[0] - mpu6050->accel_offset[0]) / mpu6050->accel_sensitivity;
+	mpu6050->accel[1] = (float)(mpu6050->accel_raw[1] - mpu6050->accel_offset[1]) / mpu6050->accel_sensitivity;
+	mpu6050->accel[2] = (float)(mpu6050->accel_raw[2] - mpu6050->accel_offset[2]) / mpu6050->accel_sensitivity;
 
 	mpu6050->gyro_angel[0] += mpu6050->gyro[0] * dt;
 	mpu6050->gyro_angel[1] += mpu6050->gyro[1] * dt;
 	mpu6050->gyro_angel[2] += mpu6050->gyro[2] * dt;
+	HMC5883L_CalculateHeading(hmc);
+
 	mpu6050->gyro_angel_f[0] = alpha * (mpu6050->gyro_angel_f[0] + mpu6050->gyro[0] * dt) + (1.0f - alpha) * acc_roll;
     mpu6050->gyro_angel_f[1] = alpha * (mpu6050->gyro_angel_f[1] + mpu6050->gyro[1] * dt) + (1.0f - alpha) * acc_pitch;
-    mpu6050->gyro_angel_f[2] = mpu6050->gyro_angel[2];
+    mpu6050->gyro_angel_f[2] = alpha_heading * (mpu6050->gyro_angel_f[2] + mpu6050->gyro[2] * dt) + (1.0f - alpha_heading) * hmc->heading_deg;
 
 	printf("gyro x: %f, gyro y: %f, gyro z: %f, accel x: %f, accel y: %f, accel z: %f, filtered_gyro x: %f, filtered_gyro y: %f, filtered_gyro z: %f, accel roll: %f, accel pitch: %f\n",
 						mpu6050->gyro_angel[0], mpu6050->gyro_angel[1], mpu6050->gyro_angel[2], 
@@ -43,10 +53,11 @@ void Calculate_Euler_Angles(MPU6050_t *mpu6050, uint32_t tick) {
 						acc_roll, acc_pitch);
 }
 
-void HMC5883L_CalculateHeading(HMC5883L_t *hmc) {
-
-    float x = (float)hmc->x;
-    float y = (float)hmc->y;
+void HMC5883L_CalculateHeading(HMC5883L_t *hmc)
+{
+    float mx = (float)hmc->x;
+    float my = (float)hmc->y;
+    float mz = (float)hmc->z;
 
     // ---------- APPLY HARD IRON CORRECTION ----------
     mx -= hmc->offset[0];
@@ -63,16 +74,16 @@ void HMC5883L_CalculateHeading(HMC5883L_t *hmc) {
     // ---------- 2D HEADING Calculation ----------
     float heading = atan2(my, mx);
 
-    // İstanbul manyetik sapma ~ +4.43°
-    heading += 4.43 * (M_PI / 180.0);
+    // Istanbul declination: +4.43°
+    heading += (4.43f * (M_PI / 180.0f));
 
-    // normalization
+    // Normalization
     if (heading < 0)
-        heading += 2 * M_PI;
-    if (heading > 2 * M_PI)
-        heading -= 2 * M_PI;
+        heading += 2.0f * M_PI;
+    if (heading > 2.0f * M_PI)
+        heading -= 2.0f * M_PI;
 
-    hmc->HeadingDegrees = heading * 180.0 / M_PI;
+    hmc->heading_deg = heading * RAD_TO_DEG;
 
-    printf("Heading: %.2f°\n", hmc->HeadingDegrees);
+    printf("Heading: %2f° (calibrated)\n", hmc->heading_deg);
 }
